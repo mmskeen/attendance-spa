5e2ecb0926619dafbe078f23cdb3bff2eb918d4e
Michael Skeen <mmskeen@gmail.com>
2020-04-21 15:07:00 -0600

    trying to get heroku to work

--- api/app.js
+++ api/app.js
@@ -10,16 +10,17 @@ const express = require("express"),
 
 // Serve static files from the React frontend app
 app.use(express.static(path.join(__dirname, '../frontend/build')))
-// Anything that doesn't match the above, send back index.html
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '../frontend/build/index.html'))
-})
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/api/meetings', meetingRoutes);
 app.use('/api/users', userRoutes);
 
+// Anything that doesn't match the above, send back index.html
+// Be sure this statement goes AFTER the api route stuff!!!
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '../frontend/build/index.html'))
+})
 
 app.listen(port, function (err) {
   if (!err) {
