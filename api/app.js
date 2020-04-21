6ba53fd5a9d080336e1f3dfb9aa238cd1d8b80ef
Michael Skeen <mmskeen@gmail.com>
2020-04-21 15:16:25 -0600

    small tweak - re-load attended meetings after hosted meetings changes

--- api/app.js
+++ api/app.js
@@ -8,19 +8,22 @@ const express = require("express"),
   userRoutes = require('./routes/users');
 
 
-// Serve static files from the React frontend app
-app.use(express.static(path.join(__dirname, '../frontend/build')))
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/api/meetings', meetingRoutes);
 app.use('/api/users', userRoutes);
+
+// Serve static files from the React frontend app
+app.use(express.static(path.join(__dirname, '../frontend/build')))
+
 
 // Anything that doesn't match the above, send back index.html
 // Be sure this statement goes AFTER the api route stuff!!!
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname + '../frontend/build/index.html'))
 })
+
+
 
 app.listen(port, function (err) {
   if (!err) {
